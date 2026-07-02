0a8d2bf7ce3f7b9b7375904dbb45e99272845ca1
shankruganganahalli003-cpu <shankruganganahalli003@gmail.com>
2026-07-02 13:22:34 +0530

    Update project

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ const cors = require("cors");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.use(
   cors({
@@ -27,6 +27,7 @@ app.use(
     crossOriginOpenerPolicy: false,
   })
 );
+
 
 
 app.use("/api/verify", emailRouter);
