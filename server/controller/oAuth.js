f76b4a6a0483f81168a66c03584068c553a34f94
shankruganganahalli003-cpu <shankruganganahalli003@gmail.com>
2026-07-02 11:31:27 +0530

    Update project

--- server/controller/oAuth.js
+++ server/controller/oAuth.js
@@ -1,12 +1,8 @@
-const jwt = require("jsonwebtoken");
-const User = require("../models/user");
-const { OAuth2Client } = require("google-auth-library");
-
-const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
-
 exports.googleLogin = async (req, res) => {
   try {
-    const { token,role } = req.body;
+    console.log("Request Body:", req.body);
+
+    const { token, role } = req.body;
 
     if (!token) {
       return res.status(400).json({ error: "Token missing" });
@@ -16,33 +12,38 @@ exports.googleLogin = async (req, res) => {
       return res.status(400).json({ error: "Role is required" });
     }
 
+    console.log("Verifying token...");
 
-    // ✅ FIX: verify WITH audience
     const ticket = await client.verifyIdToken({
       idToken: token,
       audience: process.env.GOOGLE_CLIENT_ID,
     });
 
+    console.log("Token verified");
+
     const payload = ticket.getPayload();
 
-    if (!payload?.email) {
-      return res.status(401).json({ error: "Invalid Google token" });
-    }
+    console.log("Payload:", payload);
 
     let user = await User.findOne({ email: payload.email });
+
+    console.log("User:", user);
 
     if (!user) {
       user = await User.create({
         name: payload.name,
         email: payload.email,
         googleId: payload.sub,
-        picture: payload.picture,
-        role: role
+        profileImage: payload.picture,
+        role,
       });
+
+      console.log("User created");
     } else {
-  user.role = role;
-  await user.save();
-}
+      user.role = role;
+      await user.save();
+      console.log("User updated");
+    }
 
     const jwtToken = jwt.sign(
       { id: user._id, role: user.role },
@@ -61,12 +62,12 @@ exports.googleLogin = async (req, res) => {
       success: true,
       user,
     });
+
   } catch (err) {
-  console.log("OAuth error:", err.message);
-  return res.status(401).json({
-    error: "Google login failed",
-  });
-}
+    console.error("FULL ERROR:");
+    console.error(err);
+    return res.status(401).json({
+      error: err.message,
+    });
+  }
 };
-
-
