c06d94edf8fb0b82f4b9b52f8be02f8216bb80d1
shankruganganahalli003-cpu <shankruganganahalli003@gmail.com>
2026-07-08 08:05:13 +0530

    Update project

--- client/src/api/axios.jsx
+++ client/src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://hospital-management-shz4.onrender.com",
   withCredentials: true,
 });
 
